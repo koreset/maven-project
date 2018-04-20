97c86e908a2d6d02610a6aff63bd3e2843acbe84
Jome Akpoduado <jakpodua@thoughtworks.com>
2018-04-20 05:48:28 +0200

    Fixed checkstyle issues 1

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -5,17 +5,18 @@ package com.example;
  */
 public class Greeter {
 
-  /**
-   * This is a constructor.
-   */
-  public Greeter() {
+    /**
+     * This is a constructor.
+     */
+    public Greeter() {
 
-  }
+    }
 
-/**
- * This is a method call
- */
-  public final String greet(final String someone) {
-    return String.format("Hello, %s!", someone);
-  }
+    /**
+     * @param someone
+     * @return formatted string with Hello
+     */
+    public final String greet(final String someone) {
+        return String.format("Hello, %s!", someone);
+    }
 }
